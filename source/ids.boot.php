0c59450f0f0cb8f7c0afb6dba659a2af31d73b6c
Sean Morris <640101+seanmorris@users.noreply.github.com>
2022-01-07 14:44:09 -0500

    Heroku sessions...

--- source/ids.boot.php
+++ source/ids.boot.php
@@ -4,7 +4,6 @@ if(!empty($_ENV['REDIS_URL']))
 {
 	$redisUrlParts = parse_url($_ENV['REDIS_URL']);
 	\SeanMorris\Ids\Log::error($redisUrlParts);
-	header('Set-Cookie: PHPSESSID='.$_COOKIE["PHPSESSID"].'; SameSite=None');
 	ini_set('session.save_handler', 'redis');
 	ini_set(
 		'session.save_path'
@@ -18,3 +17,5 @@ if(!empty($_ENV['REDIS_URL']))
 }
 
 session_start();
+
+header('Set-Cookie: PHPSESSID='.$_COOKIE["PHPSESSID"].'; SameSite=None');
