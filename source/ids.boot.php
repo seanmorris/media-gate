ddac1b3110c3d8b94f85eb65706a6030cdddc8bb
Sean Morris <640101+seanmorris@users.noreply.github.com>
2022-01-07 14:04:34 -0500

    Redis sessions.

--- source/ids.boot.php
+++ source/ids.boot.php
@@ -1,2 +1,10 @@
 <?php
+
+if($_ENV['REDIS_URL'])
+{
+	$redisUrlParts = parse_url($_ENV['REDIS_URL']);
+	ini_set('session.save_handler','redis');
+	ini_set('session.save_path',"tcp://$redisUrlParts[host]:$redisUrlParts[port]?auth=$redisUrlParts[pass]");
+}
+
 session_start();
