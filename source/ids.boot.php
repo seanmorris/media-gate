2e12b5d73f45959d4714c310195dca82a60b2247
Sean Morris <640101+seanmorris@users.noreply.github.com>
2022-01-14 04:06:36 -0500

    subscriptions.

--- source/ids.boot.php
+++ source/ids.boot.php
@@ -5,7 +5,6 @@ if($redisUrl = \SeanMorris\Ids\Settings::read('REDIS', 'URL'))
 	$_ENV['REDIS_URL'] = $redisUrl;
 }
 
-
 if(!empty($_ENV['REDIS_URL']))
 {
 	$redisUrlParts = parse_url($_ENV['REDIS_URL']);
@@ -27,6 +26,7 @@ if(!empty($_ENV['REDIS_URL']))
 	\SeanMorris\Ids\Settings::register('redis', function() use($redisUrlParts){
 		$redis = new Redis();
 		$redis->connect($redisUrlParts['host'], $redisUrlParts['port']);
+		$redis->auth($redisUrlParts['pass']);
 		return $redis;
 	});
 }
